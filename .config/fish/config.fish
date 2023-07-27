f79f4640717906c18d813ac805cd505c6e5681ba
Rafael Rondão <rafael.rondao@gmail.com>
2023-07-27 12:06:31 -0300

    Remove wal coloring scheme
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ set fish_greeting
 # Commands to run in interactive sessions can go here.
 if status is-interactive
     # Apply pywal color scheme.
-    cat ~/.cache/wal/sequences
+    # cat ~/.cache/wal/sequences
     # Run starship.
     starship init fish | source
 end
