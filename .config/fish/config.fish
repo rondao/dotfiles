49c2a5d52fbf84fb61b8e2797a25f5f0de5ff638
Rafael Rondão <rafael.rondao@gmail.com>
2025-11-14 22:05:15 -0300

    Add navi, sk and zoxide to fish config
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,13 @@ set fish_greeting
 
 # Commands to run in interactive sessions can go here.
 if status is-interactive
-    # Apply pywal color scheme.
-    # cat ~/.cache/wal/sequences
-    # Run starship.
+    navi widget fish | source
+    bind ctrl-space _navi_smart_replace
+    bind -M insert ctrl-space _navi_smart_replace
+
+    sk --shell fish | source
+    skim_key_bindings
+
     starship init fish | source
+    zoxide init --cmd cd fish | source
 end
